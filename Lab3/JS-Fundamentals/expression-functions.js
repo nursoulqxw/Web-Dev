9ba7a524d1f380d9fc68f1447d7570841a941dff
Nurislam Aliyev <159172371+nursoulqxw@users.noreply.github.com>
2025-02-09 08:30:50 +0500

    Add files via upload

---
+++ Lab3/JS-Fundamentals/expression-functions.js
@@ -0,0 +1,84 @@
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   function showOk() {
+//     alert( "You agreed." );
+//   }
+  
+//   function showCancel() {
+//     alert( "You canceled the execution." );
+//   }
+  
+//   // usage: functions showOk, showCancel are passed as arguments to ask
+//   ask("Do you agree?", showOk, showCancel);
+
+//   function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no();
+// }
+
+// ask(
+//   "Do you agree?",
+//   function() { alert("You agreed."); },
+//   function() { alert("You canceled the execution."); }
+// );
+
+
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   ask(
+//     "Do you agree?",
+//     function() { alert("You agreed."); },
+//     function() { alert("You canceled the execution."); }
+//   );
+
+
+
+// let age = prompt("What is your age?", 18);
+
+// let welcome;
+
+// if (age < 18) {
+
+//   welcome = function() {
+//     alert("Hello!");
+//   };
+
+// } else {
+
+//   welcome = function() {
+//     alert("Greetings!");
+//   };
+
+// }
+
+// welcome(); // ok now
+
+
+
+
+// let age = prompt("What is your age?", 18);
+
+// let welcome;
+
+// if (age < 18) {
+
+//   welcome = function() {
+//     alert("Hello!");
+//   };
+
+// } else {
+
+//   welcome = function() {
+//     alert("Greetings!");
+//   };
+
+// }
+
+// welcome(); // ok now
