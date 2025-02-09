7c53b095b02898528c07688150d55958a485b1db
Nurislam Aliyev <159172371+nursoulqxw@users.noreply.github.com>
2025-02-09 17:14:35 +0500

    Add files via upload

---
+++ Lab3/JS-Fundamentals/specials.js
@@ -0,0 +1,25 @@
+
+//declaration
+function sum(a,b){
+    let res = a+b;
+    return res;
+}
+
+// sum(1,2);
+
+//Exprassion
+let sum = function(a,b) {
+    let res = a + b;
+    return res;
+}
+
+
+let sum = (a,b) => a+b;
+
+let sum = (a,b) => {
+    return a + b;    
+}
+
+let sayHi = () => alert("HelloHi");
+
+let double = n => n * 2;
